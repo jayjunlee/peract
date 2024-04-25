0100a212d98c21d5488f5352ab93abb51bbe0f29
Jayjun Lee <jayjun@umich.edu>
2024-04-24 22:44:15 -0400

    add keypoint discovery v2

--- helpers/demo_loading_utils.py
+++ helpers/demo_loading_utils.py
@@ -17,6 +17,11 @@ def _is_stopped(demo, i, obs, stopped_buffer, delta=0.1):
                (not next_is_not_final) and gripper_state_no_change)
     return stopped
 
+def is_gripper_open(gripper_joint_positions):
+    if gripper_joint_positions[0] < 0.0399 and gripper_joint_positions[1] < 0.0399:
+        return False
+    else:
+        return True
 
 def keypoint_discovery(demo: Demo,
                        stopping_delta=0.1,
@@ -26,16 +31,17 @@ def keypoint_discovery(demo: Demo,
         prev_gripper_open = demo[0].gripper_open
         stopped_buffer = 0
         for i, obs in enumerate(demo):
+            print(i, obs.gripper_open)
+            print(dir(obs))
+
             stopped = _is_stopped(demo, i, obs, stopped_buffer, stopping_delta)
             stopped_buffer = 4 if stopped else stopped_buffer - 1
             # If change in gripper, or end of episode.
             last = i == (len(demo) - 1)
-            if i != 0 and (obs.gripper_open != prev_gripper_open or
-                           last or stopped):
+            if i != 0 and (obs.gripper_open != prev_gripper_open or last or stopped):
                 episode_keypoints.append(i)
             prev_gripper_open = obs.gripper_open
-        if len(episode_keypoints) > 1 and (episode_keypoints[-1] - 1) == \
-                episode_keypoints[-2]:
+        if len(episode_keypoints) > 1 and (episode_keypoints[-1] - 1) == episode_keypoints[-2]:
             episode_keypoints.pop(-2)
         logging.debug(f'Found {len(episode_keypoints)} keypoints.')
         return episode_keypoints
@@ -60,6 +66,41 @@ def keypoint_discovery(demo: Demo,
     else:
         raise NotImplementedError
 
+def is_gripper_open_v2(gripper_touch_forces, gripper_open):
+    if np.sum(np.abs(gripper_touch_forces)) > 0.7 and gripper_open==1.0:
+        return False
+    else:
+        return True
+
+def keypoint_discovery_v2(demo: Demo,
+                       stopping_delta=0.1,
+                       method='heuristic') -> List[int]:
+    episode_keypoints = []
+    if method == 'heuristic':
+        prev_gripper_open = demo[0].gripper_open
+        stopped_buffer = 0
+        force_vec = np.array([0, 0, 0, 0 ,0 , 0])
+        for i, obs in enumerate(demo):
+            stopped = _is_stopped(demo, i, obs, stopped_buffer, stopping_delta)
+            stopped_buffer = 4 if stopped else stopped_buffer - 1
+            # If change in gripper, or end of episode.
+            last = i == (len(demo) - 1)
+            # print(dir(obs))
+            print(i, np.sum(np.abs(obs.gripper_touch_forces)), obs.gripper_open)
+            gripper_open = is_gripper_open(obs.gripper_joint_positions)
+            if np.sum(np.abs(obs.gripper_touch_forces)) > 0.7 and gripper_open is True:
+                demo[i].gripper_open = False
+                gripper_open = False
+            if i != 0 and (gripper_open != prev_gripper_open or last or stopped):
+                # print(obs.gripper_open, prev_gripper_open)
+                episode_keypoints.append(i)
+            prev_gripper_open = gripper_open
+        if len(episode_keypoints) > 1 and (episode_keypoints[-1] - 1) == episode_keypoints[-2]:
+            episode_keypoints.pop(-2)
+        logging.debug(f'Found {len(episode_keypoints)} keypoints.')
+        return episode_keypoints
+    else:
+        raise NotImplementedError
 
 # find minimum difference between any two elements in list
 def find_minimum_difference(lst):
